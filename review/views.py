b11011386490f5f6655bb4f787466363f3c1c3af
KittySwe <kaythiswetun@gmail.com>
2019-07-18 20:54:23 +0800

    ui modification

--- review/views.py
+++ review/views.py
@@ -9,9 +9,17 @@ def index(request):
     context = {}
     category = Category.objects.all()
 
+    cat_dict = dict()
+    print(category.get(Cid=1))
+
+
+    for dd in category:
+        print(dd.Cid)
+        cat_dict[dd.Cid] = dd.Cname
+
     weather = Restaurant.objects.all()
     context = {
-        'cat': category,
+        'cat': cat_dict,
         'restaurant': weather,
     }
 
